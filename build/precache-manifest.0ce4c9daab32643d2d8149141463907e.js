7c08794a08c3842089149e847a6f4c99f056c997
Shuihua Li <lishuihua@rd.netease.com>
2018-11-13 10:42:10 +0800

    add: build

---
+++ build/precache-manifest.0ce4c9daab32643d2d8149141463907e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e16d02e0dcc5c463fbb",
+    "url": "./static/js/main.0e16d02e.chunk.js"
+  },
+  {
+    "revision": "bfab218206f92345ae17",
+    "url": "./static/js/1.bfab2182.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "bca1d26f12d1b82eed24beeadb2b1263",
+    "url": "./static/media/example.bca1d26f.jpg"
+  },
+  {
+    "revision": "24ac5425175504cc7cc79e43dd1d7d6b",
+    "url": "./index.html"
+  }
+];
